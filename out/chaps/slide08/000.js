e615165e0dbec17f32180ba2d12b09f865da19ac
Mohammed Hrima <mhrima@e1r10p4.1337.ma>
2025-05-19 02:28:34 +0100

    clean

--- out/chaps/slide08/000.js
+++
@@ -1,3 +0,0 @@
-element("div", { class: "slide-content" },
-    element("h1", null, "Reconciliation:"),
-    element("p", null, "It's how the framework updates the real DOM by comparing changes in the virtual DOM."));
